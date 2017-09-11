8e85f73e8a91a84dc13fda60ab40cdccefedc890
Anupure <anupdutta96@gmail.com>
2017-09-12 04:54:28 +0530

    emit new message

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function() {
   console.log('connected to server');
 
-  socket.emit('createMessage', {
-    from: 'Andrew',
-    text: 'heya'
-  });
 });
 socket.on('disconnect', function(){
   console.log('disconnected');

622493c009a219993195fbfe6f176c123830ea60
Anupure <anupdutta96@gmail.com>
2017-09-12 05:24:07 +0530

    greet new user

--- server/server.js
+++ server/server.js
@@ -14,15 +14,31 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log("new user");
 
+  socket.emit('newMessage', {
+     from: 'Admin',
+     text: 'welcome to chat room',
+     createdAt:new Date().getTime()
+     });
+
+     socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'new user joined',
+        createdAt:new Date().getTime()
+       });
 
   socket.on('createMessage', (message) =>{
     console.log('createMessage', message);
     io.emit('newMessage', {
       from: message.from,
-      text: message.text,
-      createdAt:new Date().getTime()
+       text: message.text,
+       createdAt:new Date().getTime()
 
-    });
+     });
+    // socket.broadcast.emit('newMessage', {
+    //    from: message.from,
+    //    text: message.text,
+    //   createdAt:new Date().getTime()
+    //   });
   });
 
   socket.on('disconnect', () => {

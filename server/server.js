a5e88673cbe3a8d89f15e1b4b5f9e04077d6a504
Anupure <anupdutta96@gmail.com>
2017-09-12 04:00:03 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
+io.on('connection', (socket) => {
+  console.log("new user");
 
-app.listen(port, () => {
+  socket.on('disconnect', () => {
+    console.log('disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`server running on ${port} ` );
 })
